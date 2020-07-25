5be962fe1e33229d7a7c0a61e56ecc30529fae09
Agarwal, Pankaj <2005pank@gmail.com>
2020-07-25 23:53:39 +0530

    Linux Core Concepts

---
+++ ShellScript/create-and-launch-rocket.sh
@@ -0,0 +1,11 @@
+mkdir lunar-mission
+
+rocket-add lunar-mission
+
+rocket-start-power lunar-mission
+rocket-internal-power lunar-mission
+rocket-start-sequence lunar-mission
+rocket-start-engine lunar-mission
+rocket-lift-off lunar-mission
+
+rocket-status lunar-mission
